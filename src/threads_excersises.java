8fcbd3c52f87d9dd7dbe7c773d6707cb3f35ffb2
gahson <kacpergarus12@gmail.com>
2024-10-03 00:03:30 +0200

    creating 5 threads

--- src/threads_excersises.java
+++ src/threads_excersises.java
@@ -1,17 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+class MyRunnable implements Runnable {
+    private int threadId;
+
+    public MyRunnable(int id) {
+        this.threadId = id;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Wątek " + threadId + " działa");
+    }
+}
+
 public class threads_excersises {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            MyRunnable myRunnable = new MyRunnable(i);
+            Thread thread = new Thread(myRunnable);
+            thread.start();
         }
     }
 }
