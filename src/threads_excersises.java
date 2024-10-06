f7d1367627d8225c9bf153b73225abb31436dc59
gahson <kacpergarus12@gmail.com>
2024-10-07 00:54:24 +0200

    comparing concurrency errors with different nums of loops and threads

--- src/threads_excersises.java
+++ src/threads_excersises.java
@@ -1,43 +1,46 @@
 public class threads_excersises {
-    private static int w=0;
-    private static final int NUM_OF_LOOPS=10;
-    private static final int NUM_OF_THREADS=10;//needs to be even to have same amount od decreasing and increasing threads
 
+    private static int w=0;
     static class increasingThreads implements Runnable {
 
-
+        private int numOfLoops;
+        public increasingThreads(int numOfLoops){
+            this.numOfLoops=numOfLoops;
+        }
         @Override
         public void run() {
-            for (int i=0;i<NUM_OF_LOOPS;i++){
+            for (int i=0;i<numOfLoops;i++){
                 w++;
             }
         }
     }
 
     static class decreasingThreads implements Runnable {
-
-
+        private int numOfLoops;
+        public decreasingThreads(int numOfLoops){
+            this.numOfLoops=numOfLoops;
+        }
         @Override
         public void run() {
-            for (int i=0;i<NUM_OF_LOOPS;i++){
+            for (int i=0;i<numOfLoops;i++){
                 w--;
             }
         }
     }
 
-    public static void main(String[] args) {
-        Thread[] threads=new Thread[NUM_OF_THREADS];
-        for (int i=0;i<NUM_OF_THREADS;i++){
+    public static void runThreads(int numOfThreads, int numOfLoops) {
+        Thread[] threads=new Thread[numOfThreads];
+        for (int i=0;i<numOfThreads;i++){
             if (i%2==0){
-                threads[i]=new Thread(new increasingThreads());
+                threads[i]=new Thread(new increasingThreads(numOfLoops));
             }
             else {
-                threads[i]=new Thread(new decreasingThreads());
+                threads[i]=new Thread(new decreasingThreads(numOfLoops));
             }
             threads[i].start();
         }
 
-        for (int i=0;i<NUM_OF_THREADS;i++){
+        for (int i=0;i<numOfThreads;i++){
             try {
                 threads[i].join();
             } catch (InterruptedException e) {
@@ -47,5 +50,14 @@ public class threads_excersises {
         System.out.println(w);
 
 
+    }
+    public static void main(String[] args){
+        int numOfOperations=2*(int)Math.pow(10,5);//for the tests it will be easier for it to be power of ten
+
+        for (int i=2;i<=numOfOperations;i*=10){
+            System.out.printf("Threads:%d   Loops:%d    Value:",i,numOfOperations/i);
+            runThreads(i,numOfOperations/i);
+            w=0;
+        }
     }
 }
